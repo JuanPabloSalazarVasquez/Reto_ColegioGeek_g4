ca99c0fec0b0146afca095c063813af6822bb403
Emanuel684 <65985811+Emanuel684@users.noreply.github.com>
2021-01-28 16:38:48 -0500

    Se agregan cambios en el backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-
+app.get('/', (req, res) => {
+    res.send('Servidor barppi corriendo');
+});
 
 app.set('port', process.env.PORT || 6666);
 app.listen(app.get('port'), () => {
-    console.log(`Server on port:${app.get('port')}`);
+    console.log(`Server on port: ${app.get('port')}`);
 });
