21989d220b78a473c744d3e23eeb80f1b20fbfab
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-01-28 16:18:17 -0500

    Iniciando

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -4,12 +4,14 @@ import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import Main1 from '../Components/Main1';
 
+
+
 function Home() {
   return (
     <>
-    <Header />
-    <Main1 />
-    <Footer />
+      <Header />
+      <Main1 />
+      <Footer />
     </>
   );
 }
