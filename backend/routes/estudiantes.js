dcca1cda52ef06bb25371f7e3b4ab529d8fa3da2
Brayan <brayancreepy25@gmail.com>
2021-01-29 16:42:54 -0500

    se elimino el frontend y se agregan algunas peticiones

--- backend/routes/estudiantes.js
+++ backend/routes/estudiantes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express'); 
 const estudiantes = Router();
 
-estudiantes.post('/nueva-materia', (req, res) => {
+estudiantes.post('/nuevo-estudiante', (req, res) => {
     const {id_persona, grado,codigo_estudiante}=req.body; 
 
     const estudiante=[id_persona, grado,codigo_estudiante];
