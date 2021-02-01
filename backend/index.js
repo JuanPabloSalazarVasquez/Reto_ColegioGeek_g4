4e56711b8bce1d2bf5e44b81ca22c0a7226fc14c
Emanuel684 <65985811+Emanuel684@users.noreply.github.com>
2021-01-31 20:38:09 -0500

    Se agregan algunas rutas para consultas.

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const maestro = require("./routes/maestro");
 const materias = require("./routes/materias");
 const notas = require("./routes/notas");
 const persona = require("./routes/persona");
+const grupos_materias = require('./routes/grupos-materias');
+const grupos_estudiantes = require('./routes/grupos-estudiantes');
+const consolidados = require('./routes/consolidados');
 //Fin de inicialización de las rutas
 const app = express();
 
@@ -28,6 +31,9 @@ app.use("/maestro", maestro);
 app.use("/materias", materias);
 app.use("/notas", notas);
 app.use("/persona", persona);
+app.use('/grupos-materias', grupos_materias);
+app.use('/grupos-estudiantes', grupos_estudiantes);
+app.use('/consolidados', consolidados);
 //Fin configuración de rutas
 
 app.set("port", process.env.PORT || 4535);
---
+++ backend/routes/consolidados.js
@@ -0,0 +1,35 @@
+const { Router } = require('express'); 
+const consolidados = Router();
+const pool = require('./db/db');
+
+// Crear un nuevo registro en la tabla notas
+consolidados.post('/nueva-nota', (req,res)=>{
+    const { id_materia,id_grupo,id_estudiante,nota,tipo_nota } = req.body;
+    const notas = [ id_materia,id_grupo,id_estudiante,nota,tipo_nota ];
+
+    const nuevaNotas = `INSERT INTO notas(id_materia,id_grupo,id_estudiante,nota,tipo_nota) VALUES (?,?,?,?,?)`;
+
+    pool.query(nuevaNotas, notas, (err, results, fields)=>{
+        if(err){
+            return console.error(err.message);
+        }else{
+            res.json({message: `se creo una nueva nota`});
+        }
+    });
+});
+// Fin crear un nuevo registro en la tabla notas
+
+// Consultar todas la notas de la tabla notas
+consolidados.get('/all-notas', (req,res)=>{
+    pool.query('SELECT * FROM notas', (err, rows, fields)=>{
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+// Fin consultar todas la notas de la tabla notas
+
+
+module.exports = consolidados;
